74bb23cc87665bba530a058f380f8e75adc0a0c0
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-12-27 23:10:14 +0900

    [変更] 奇数・偶数判定プログラム

--- helloworld.go
+++ helloworld.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello World")
+	for i := 0; i <= 100; i = i + 1 {
+		if i%2 == 0 {
+			println("%d-偶数", i)
+		} else {
+			println("%d-奇数", i)
+		}
+	}
 }
